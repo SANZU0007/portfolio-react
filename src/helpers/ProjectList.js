bba417db3a0b0251346f47d7838d8fe5afdb928b
sanjay <LENOVO@DESKTOP-AKPL9L4.(none)>
2023-09-07 14:19:25 +0530

    last commit

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,9 +1,9 @@
 import Proj1 from "../assets/proj1.jpg";
 
 import Proj3 from "../assets/proj3.jpg";
-import Proj4 from "../assets/proj4.avif";
+
 import Proj5 from "../assets/proj5.jpg";
-import Proj6 from "../assets/proj6.jpg";
+
 
 export const ProjectList = [
   {
@@ -60,13 +60,7 @@ export const ProjectList = [
     link2: "https://github.com/SANZU0007/Password-Reset-Frontend.git",
     link3: "https://github.com/SANZU0007/Password-Reset-backend.git	",
   },
-  {
-    name: "Price cart",
-    image: Proj4,
-    skills: "react.js",
-    link: "https://stirring-dragon-cb0c60.netlify.app/",
-    link2: "https://github.com/SANZU0007/Cart-Itemcount.git",
-  },
+  
   {
     name: "Dad Jokes",
     image: Proj5,
@@ -74,11 +68,5 @@ export const ProjectList = [
     link: "https://playful-brioche-93ea77.netlify.app/",
     link2: "https://github.com/SANZU0007/DAD-jokes-API-00.git",
   },
-  {
-    name: "Calculator",
-    image: Proj6,
-    skills: "React Native,JavaScript,HTML,CSS",
-    link: "https://mellifluous-brigadeiros-91bc33.netlify.app/",
-    link2: "https://github.com/SANZU0007/calculator-respoosive.git",
-  },
+ 
 ];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,64 +1,109 @@
 import React from "react";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import  TwitterIcon from "@material-ui/icons/Twitter";
+import TwitterIcon from "@material-ui/icons/Twitter";
 import GithubIcon from "@material-ui/icons/GitHub";
 import "../styles/Home.css";
 import sanjay001 from "../assets/sanjay001.jpg";
+import Projects from "../pages/Projects";
 
 function Home() {
   return (
     <div className="home">
       <div className="about">
-     
         <img
           src={sanjay001}
           alt="Profile"
-          className="profile-image" id="profile-image1"
+          className="profile-image"
+          id="profile-image1"
         />
-       <h5 className="sanjay"> Hi, My Name  is  <b className="blinking-text">sanjay</b></h5>
+        <h3 className="sanjay">
+          Hi, My Name is <b className="blinking-text">sanjay</b>
+        </h3>
 
         <div className="prompt">
-          <p>A Full Stack web developer with a passion for learning and creating.</p>
+          <h4 className="developer">
+            A Full Stack web developer with a passion for{" "}
+            <b> learning and creating.</b>
+          </h4>
           <div id="iconesss">
-
-          <a href="https://www.linkedin.com/in/sanjay-m-sanjay-89688825a/" target="_blank" rel="noopener noreferrer">
-          <LinkedInIcon />
-        </a>
-        <a href="https://twitter.com/sanjay_1119" target="_blank" rel="noopener noreferrer">
-        <TwitterIcon/>
-</a>
-
-
-        <a href="https://github.com/SANZU0007" target="_blank" rel="noopener noreferrer">
-          <GithubIcon />
-        </a>
-        </div>
-
+            <a
+              href="https://www.linkedin.com/in/sanjay-m-sanjay-89688825a/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <LinkedInIcon />
+            </a>
+            <a
+              href="https://twitter.com/sanjay_1119"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <TwitterIcon />
+            </a>
+            <a
+              href="https://github.com/SANZU0007"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <GithubIcon />
+            </a>
+          </div>
         </div>
       </div>
       <div className="skills">
-        <h1 className="skiles"> Skills</h1>
-        <ol className="list">
-          <li className="item">
-            <h2> Front-End</h2>
-            <span>
-              ReactJS, Redux, HTML, CSS, React Native, NPM,
-              Ionic, BootStrap, MaterialUI, TailwindCSS, StyledComponents
-            </span>
-          </li>
-          <li className="item">
-            <h2>Back-End</h2>
-            <span>
-              NodeJS, ExpressJS,
-              MySQL, MongoDB, DynamoDB, AWS S3
-            </span>
-          </li>
-          <li className="item">
-            <h2>Languages</h2>
-            <span>JavaScript</span>
-          </li>
-        </ol>
+        <h1 className="skills">Skills</h1>
+
+        <div className="skills-Logo">
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=20909&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=21278&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=PXTY4q2Sq2lG&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=hsPbhkOH4FMe&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            id="skills-Logo2"
+            src="https://img.icons8.com/?size=1x&id=123603&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=gFw7X5Tbl3ss&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=jD-fJzVguBmw&format=png"
+            alt="no image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=kg46nzoJrmTR&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=20906&format=png"
+            alt="no"
+          ></img>
+          <img className="skills-Logo1" src="https://img.icons8.com/?size=1x&id=74402&format=png"></img>
+        </div>
       </div>
+      <Projects />
     </div>
   );
 }
