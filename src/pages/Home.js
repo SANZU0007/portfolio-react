3050ffd9666e6e0fdc14be32e6dd972b2b58a8f4
sanjay <LENOVO@DESKTOP-AKPL9L4.(none)>
2023-09-17 18:55:28 +0530

    new

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,28 @@
 import React from "react";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
-import TwitterIcon from "@material-ui/icons/Twitter";
-import GithubIcon from "@material-ui/icons/GitHub";
 import "../styles/Home.css";
 import sanjay001 from "../assets/sanjay001.jpg";
 import Projects from "../pages/Projects";
 
 function Home() {
-  const resumeURL = 
-    'https://drive.google.com/file/d/1chyrDO1fRl06obFWijJO_gzrgk0k-YnD/view?usp=drivesdk';
+  const resumeURL =
+    "https://drive.google.com/file/d/1chyrDO1fRl06obFWijJO_gzrgk0k-YnD/view?usp=drivesdk";
+
+  const openEmailClient = () => {
+    const subject = "Contact Request"; // Email subject
+    const email = "sanjaykuttyags@gmail.com"; // Your email address
+
+    const body = `
+    Hi,i am sanjay
+
+    I'd like to get in touch with you.`;
+
+    const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
+      subject
+    )}&body=${encodeURIComponent(body)}`;
+
+    // Open the user's default email client
+    window.location.href = mailtoLink;
+  };
 
   return (
     <div className="home">
@@ -25,56 +39,81 @@ function Home() {
 
         <div className="prompt">
           <h4 className="developer">
-            A Full Stack web developer with a passion for <b>learning and creating.</b>
+            A Full Stack web developer with a passion for{" "}
+            <b>learning and creating.</b>
           </h4>
-         
         </div>
       </div>
       <div className="skills">
         <h1 className="skills-heading">Skills</h1>
         <div className="skills-Logo">
-          {/* Your skill icons */}
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=20909&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=21278&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=PXTY4q2Sq2lG&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=hsPbhkOH4FMe&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            id="skills-Logo2"
+            src="https://img.icons8.com/?size=1x&id=123603&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=gFw7X5Tbl3ss&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=jD-fJzVguBmw&format=png"
+            alt="no image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=kg46nzoJrmTR&format=png"
+            alt="no-image"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=20906&format=png"
+            alt="no"
+          ></img>
+          <img
+            className="skills-Logo1"
+            src="https://img.icons8.com/?size=1x&id=74402&format=png"
+          ></img>
         </div>
       </div>
-      <div>
-       
-      </div>
       <Projects />
-      <div>
-        <h1 className="resume-style">Contact Me</h1>
-        <p>
-          <a
-            href={resumeURL}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Downloaded CV
-          </a>
-        </p>
-        <div id="iconesss">
-            <a
-              href="https://www.linkedin.com/in/sanjay-m-sanjay-89688825a/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <LinkedInIcon />
-            </a>
-            <a
-              href="https://twitter.com/sanjay_1119"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <TwitterIcon />
-            </a>
-            <a
-              href="https://github.com/SANZU0007"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <GithubIcon />
-            </a>
-          </div>
-      </div>
+      <div className="contact-details">
+  <h1 className="resume-style">Contact Me</h1>
+
+  <button  role="img" aria-label="Email" onClick={openEmailClient}> &#9993; "click to Send Email"</button>
+ <p>
+ <button id="cv-1"> 
+  <a href={resumeURL} target="_blank" rel="noopener noreferrer">
+    Resume CV
+  </a>
+  </button>
+  </p>
+  <p id="call-1"> &#9742; +91 78680 52815</p>
+</div>
+
     </div>
   );
 }
