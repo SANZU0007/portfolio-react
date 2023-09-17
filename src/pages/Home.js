c3d062424369f887cf89d491a3473cfce5f08b21
sanjay <LENOVO@DESKTOP-AKPL9L4.(none)>
2023-09-17 06:46:11 +0530

    new

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,9 @@ import sanjay001 from "../assets/sanjay001.jpg";
 import Projects from "../pages/Projects";
 
 function Home() {
+  const resumeURL = 
+    'https://drive.google.com/file/d/1chyrDO1fRl06obFWijJO_gzrgk0k-YnD/view?usp=drivesdk';
+
   return (
     <div className="home">
       <div className="about">
@@ -22,10 +25,33 @@ function Home() {
 
         <div className="prompt">
           <h4 className="developer">
-            A Full Stack web developer with a passion for{" "}
-            <b> learning and creating.</b>
+            A Full Stack web developer with a passion for <b>learning and creating.</b>
           </h4>
-          <div id="iconesss">
+         
+        </div>
+      </div>
+      <div className="skills">
+        <h1 className="skills-heading">Skills</h1>
+        <div className="skills-Logo">
+          {/* Your skill icons */}
+        </div>
+      </div>
+      <div>
+       
+      </div>
+      <Projects />
+      <div>
+        <h1 className="resume-style">Contact Me</h1>
+        <p>
+          <a
+            href={resumeURL}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Downloaded CV
+          </a>
+        </p>
+        <div id="iconesss">
             <a
               href="https://www.linkedin.com/in/sanjay-m-sanjay-89688825a/"
               target="_blank"
@@ -48,62 +74,7 @@ function Home() {
               <GithubIcon />
             </a>
           </div>
-        </div>
       </div>
-      <div className="skills">
-        <h1 className="skills">Skills</h1>
-
-        <div className="skills-Logo">
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=20909&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=21278&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=PXTY4q2Sq2lG&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=hsPbhkOH4FMe&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            id="skills-Logo2"
-            src="https://img.icons8.com/?size=1x&id=123603&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=gFw7X5Tbl3ss&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=jD-fJzVguBmw&format=png"
-            alt="no image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=kg46nzoJrmTR&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=20906&format=png"
-            alt="no"
-          ></img>
-          <img className="skills-Logo1" src="https://img.icons8.com/?size=1x&id=74402&format=png"></img>
-        </div>
-      </div>
-      <Projects />
     </div>
   );
 }
