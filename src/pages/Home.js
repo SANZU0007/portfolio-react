70060bdb5e617c813a0ec16af9c8d5b0b364ab96
sanjay <LENOVO@DESKTOP-AKPL9L4.(none)>
2023-09-22 15:25:54 +0530

    last

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Home.css";
 import sanjay001 from "../assets/sanjay001.jpg";
 import Projects from "../pages/Projects";
@@ -23,6 +23,28 @@ function Home() {
     window.location.href = mailtoLink;
   };
 
+  // Typewriter effect code for the developer text
+  const [typedText, setTypedText] = useState("");
+  const text =
+    "A Full Stack web developer with a passion for learning and creating.";
+
+  useEffect(() => {
+    let index = 0;
+    const typingInterval = setInterval(() => {
+      if (index < text.length) {
+        setTypedText((prevText) => prevText + text.charAt(index));
+        index++;
+      } else {
+        clearInterval(typingInterval);
+      }
+    }, 100); // Adjust typing speed (milliseconds)
+
+    // Clear the interval after typing the entire text once
+    return () => {
+      clearInterval(typingInterval);
+    };
+  }, []);
+
   return (
     <div className="home">
       <div className="about">
@@ -36,66 +58,14 @@ function Home() {
           Hi, My Name is <b className="blinking-text">Sanjay</b>
         </h3>
 
-        <div className="prompt">
-          <h4 className="developer">
-            A Full Stack web developer with a passion for <b>learning and creating.</b>
-          </h4>
+        <div className="full">
+          <div className="developer">{typedText}</div>
         </div>
       </div>
       <div className="skills">
         <h1 className="skills-heading">Skills</h1>
-        <div className="skills-Logo">
-        <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=20909&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=21278&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=PXTY4q2Sq2lG&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=hsPbhkOH4FMe&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            id="skills-Logo2"
-            src="https://img.icons8.com/?size=1x&id=123603&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=gFw7X5Tbl3ss&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=jD-fJzVguBmw&format=png"
-            alt="no image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=kg46nzoJrmTR&format=png"
-            alt="no-image"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=20906&format=png"
-            alt="no"
-          ></img>
-          <img
-            className="skills-Logo1"
-            src="https://img.icons8.com/?size=1x&id=74402&format=png"
-          ></img>
-       
+        <div className="skills-logos">
+          {/* Your skills logos */}
         </div>
       </div>
       <Projects />
@@ -105,15 +75,15 @@ function Home() {
         <button role="img" aria-label="Email" onClick={openEmailClient}>
           &#9993; Click to Send Email
         </button>
-        
+
         <p>
-          <button id="cv-1"> 
+          <button id="cv-1">
             <a href={resumeURL} target="_blank" rel="noopener noreferrer">
               Download Resume CV
             </a>
           </button>
         </p>
-        
+
         <p id="call-1"> &#9742; +91 78680 52815</p>
       </div>
     </div>
